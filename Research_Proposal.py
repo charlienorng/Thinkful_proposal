4d59be35477b16bc8e93468821dcfd0d7e67335a
charlienorng <82837420+charlienorng@users.noreply.github.com>
2021-07-19 12:31:48 -0700

    Add files via upload

---
+++ Research_Proposal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+'''Avocado farmers in California wants to increase the amount of money they make. They want to know
+what type of avocado to sell and where to sell it to make the most profit. To avoid higher shipping
+costs and export fees, they want to keep all sales within the United States. 
+    The hypotheses: Do conventional or organic avocados sell more? Where do these avocados are more
+likely to sell?
+    The data uses will be Avocado Prices (2020) from Kaggle.
+    The data is first going to be graphed and inspected visually for normality and  skewness. Then
+a t-test and a p-value will be used'''
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
